6bfb42a184c9f31da7a4c2cd53df79e2904c799b
sbz <sbz@6dev.net>
2021-03-04 17:30:50 +0000

    add check-distfile script to report distfiles issues while fetching

---
+++ check-distfile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+svnbase="/home/sbz/svn/ports"
+
+[ ! -f /usr/local/bin/distilator ] && pkg install -y distilator
+
+ports=$(make -C "${svnbase}" search key="sbz@FreeBSD.org" \
+    display=path|grep -v ^$|awk '{print $2}')
+
+for origin in ${ports};
+do
+    (cd ${origin}; echo "Port: ${origin}"; distilator; echo) | tee -a /tmp/myport-distfile.log
+done
