bfdacca5c9863fcc67bdf5a21a74cf54b6fb9bab
Sofian Brabez <sbz@6dev.net>
2023-02-04 23:29:07 +0100

    getprs: display human readable date and truncate summary

--- getprs.py
+++ getprs.py
@@ -2,8 +2,13 @@
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
-import bugzilla
+import datetime
 import os
+import sys
+from typing import List
+
+import bugzilla
+from bugzilla.bug import Bug
 
 """
 In order to use this script, complete the following instructions:
@@ -11,23 +16,53 @@ In order to use this script, complete the following instructions:
     2. Define environment variable FREEBSD_BZ_API_TOKEN in your shell
 """
 
-api_key = os.environ.get("FREEBSD_BZ_API_TOKEN")
-user = os.environ.get("USER") or "sbz"
+RFC_3339_FORMAT = "%Y%m%dT%H:%M:%S"
 
-bz = bugzilla.Bugzilla(
-    url="https://bugs.freebsd.org/bugzilla/xmlrpc.cgi",
-    api_key=api_key
-)
 
-query = bz.build_query(
-    product=["Ports & Packages","Base System"],
-    assigned_to=user,
-    status="__open__"
-)
+def fetch_bugzilla_prs(user: str, status: str = "__open__") -> List[Bug]:
+    api_key = os.environ.get("FREEBSD_BZ_API_TOKEN", None)
+    if not api_key:
+        raise ("No api token provided")
 
-bugs = bz.query(query)
-for bug in sorted(bugs, key=lambda x:x.last_change_time, reverse=True):
-    print("{:10} {:10} {:8} {:60}".format(str(bug.last_change_time), bug.id,
-                                          bug.status, bug.summary))
+    bz = bugzilla.Bugzilla(
+        url="https://bugs.freebsd.org/bugzilla/xmlrpc.cgi", api_key=api_key
+    )
 
-print("{} bugs currently opened.".format(len(bugs)))
+    query = bz.build_query(
+        product=["Ports & Packages", "Base System"],
+        assigned_to=user,
+        status=status,
+    )
+
+    bugs = bz.query(query)
+
+    return bugs
+
+
+def print_bugzilla_prs(bugs: List[Bug]) -> None:
+    for bug in sorted(bugs, key=lambda x: x.last_change_time, reverse=True):
+        bug_time = datetime.datetime.strptime(
+            str(bug.last_change_time), RFC_3339_FORMAT
+        )
+        print(
+            "{} {:10} {:12} {:60}".format(
+                bug_time,
+                bug.id,
+                bug.status,
+                bug.summary[:120] + " .." if len(bug.summary) > 120 else bug.summary,
+            )
+        )
+
+
+def main() -> int:
+    user = os.environ.get("USER") or "sbz"
+    bugs = fetch_bugzilla_prs(user)
+    print_bugzilla_prs(bugs)
+
+    print("{} bugs currently opened.".format(len(bugs)))
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
